7fce86cea9380bea5c08276f32ce708b4dd3b545
Akshay Rajasekaran <akshaytg2003@gmail.com>
2023-04-23 18:02:56 +0530

    King moves verified. Completed movement of all pieces except for special rules such as enpassant, castling and promotion. Pinning of piece works as well. Added method to check for checks.

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ import chess as ch
 
 if __name__ == '__main__':
     b = ch.board()
+    p = b.pieces
     # b.fen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
-    # b.addPiece('Rook', 'W', (2, 1))
-    # b.addPiece('Bishop', 'W', (2, 3))
-    b.addPiece('knight', 'B', ch.getPosFromStdNotation('a4'))
-    b.legalMoves(ch.getPosFromStdNotation('a4'))
+    b.addPiece(p, 'queen', 'W', ch.getPosFromStdNotation('b2'))
+    b.addPiece(p, 'king', 'W', ch.getPosFromStdNotation('a1'))
+    b.addPiece(p, 'queen', 'B', ch.getPosFromStdNotation('h8'))
+    b.legalMoves(p, ch.getPosFromStdNotation('b2'))
